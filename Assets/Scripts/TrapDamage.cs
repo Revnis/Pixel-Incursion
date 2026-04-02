6577c3fe07adf103de8aed0f283586755c1ef3ac
punnnrn <yiukpr@gmail.com>
2026-04-02 18:45:54 +0700

    TrapUpd1

---
+++ Assets/Scripts/TrapDamage.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class TrapDamage : MonoBehaviour
+{
+    [Header("Trap Settings")]
+    public int damageAmount = 10;
+    public string trapName = "Spike_Trap";
+    public float damageDelay = 1f;
+
+    private float lastDamageTime;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (Time.time >= lastDamageTime + damageDelay)
+            {
+                var player = collision.GetComponent<PlayerStats>();
+
+                if (player != null)
+                {
+                    player.TakeDamage(damageAmount, trapName);
+
+                    lastDamageTime = Time.time;
+                    Debug.Log($"Hit {trapName}");
+                }
+            }
+        }
+    }
+}
